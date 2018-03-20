f10721394c11796a331ada5b5eb32e4c035a6141
Madhusudan <smnt324@gmail.com>
2018-03-20 10:03:56 +0530

    test

--- blog/app/Http/Controllers/HomeController.php
+++ blog/app/Http/Controllers/HomeController.php
@@ -24,12 +24,32 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (!empty($_SERVER["HTTP_CLIENT_IP"]))
+{
+ //check for ip from share internet
+ $ip = $_SERVER["HTTP_CLIENT_IP"];
+}
+elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+{
+ // Check for the Proxy User
+ $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+}
+else
+{
+ $ip = $_SERVER["REMOTE_ADDR"];
+}
+echo getHostByName(getHostName());
+
+
+print "<pre>"; print_r($_SERVER);  print "</pre>";
+echo $ip; exit;
+        //return view('home');
     }
 
     public function welcomeAngular() 
     {
         //echo "here"; exit;
+
         return Response::json("Hello World");
     }
 }
